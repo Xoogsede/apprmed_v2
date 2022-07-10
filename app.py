02f4bda3625543eb0de3b42263b272cd0d7efb94
Xoogsade <abdi-jama.mahamoud@hotmail.fr>
2022-07-10 04:04:53 +0200

    App update for deployment

--- app.py
+++ app.py
@@ -4,25 +4,21 @@ from app.db_extention import db
 from flask import render_template
 from app.models import User
 
+rmedapp = create_app('config.ProductionConfig')
+with rmedapp.app_context():
+    db.create_all()
+    if not User.query.filter_by(matricule='1000000000').first():
+        User.create_user(
+            matricule='1000000000',
+            fonction = 'auxiliaire sanitaire',
+            mdp='topsecret_')
+
+@rmedapp.route('/')
+def home():
+    return render_template('home.html')   
 
 
 
-if __name__ == '__main__': 
-    rmedapp = create_app('config.ProductionConfig')
-    with rmedapp.app_context():
-        db.create_all()
-        if not User.query.filter_by(matricule='1000000000').first():
-            User.create_user(
-                matricule='1000000000',
-                fonction = 'auxiliaire sanitaire',
-                mdp='topsecret_')
 
-    @rmedapp.route('/')
-    def home():
-        return render_template('home.html')   
-
-
-    
-
-    rmedapp.run()
+rmedapp.run()
 
