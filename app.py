2b8f9344e230cade04110dc323245a01b83dd1a0
Xoogsade <abdi-jama.mahamoud@hotmail.fr>
2022-07-10 16:16:17 +0200

    app file modified

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from app.models import User
 
 rmedapp = create_app('config.DevelopmentConfig')
 with rmedapp.app_context():
-    db.create_all()
-    if not User.query.filter_by(matricule='1000000000').first():
-        User.create_user(
-            matricule='1000000000',
-            fonction = 'auxiliaire sanitaire',
-            mdp='topsecret_')
+    # db.create_all()
+    # if not User.query.filter_by(matricule='1000000000').first():
+    #     User.create_user(
+    #         matricule='1000000000',
+    #         fonction = 'auxiliaire sanitaire',
+    #         mdp='topsecret_')
     rmedapp.run()
