79cd78a1e211ed420aa784259c2598279a292ad9
Xoogsade <abdi-jama.mahamoud@hotmail.fr>
2022-07-13 15:01:52 +0200

    app config updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app.db_extention import db
 from app.models import User
 import os
 
-rmedapp = create_app('config.DevelopmentConfig')
+rmedapp = create_app('config.ProductionConfig')
 with rmedapp.app_context():
     db.create_all()
     if not User.query.filter_by(matricule='1000000000').first():
