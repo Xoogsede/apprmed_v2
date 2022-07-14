b2f3615822cdd6463aad5e49e98841bebe5d0d84
Xoogsade <abdi-jama.mahamoud@hotmail.fr>
2022-07-14 20:53:32 +0200

    app config updated

--- apprmed.py
+++ apprmed.py
@@ -5,14 +5,14 @@ from app.db_extention import db
 
 import os
 
-if __name__ == '__main__':
-    app = create_app('config.ProductionConfig')
-    with app.app_context():
-        db.create_all()
-        from app.models import User
-        if not User.query.filter_by(matricule='1000000000').first():
-            User.create_user(
-                matricule='1000000000',
-                fonction = 'auxiliaire sanitaire',
-                mdp='topsecret_')            
-        app.run()
+app = create_app('config.ProductionConfig')
+with app.app_context():
+    db.create_all()
+    from app.models import User
+    if not User.query.filter_by(matricule='1000000000').first():
+        User.create_user(
+            matricule='1000000000',
+            fonction = 'auxiliaire sanitaire',
+            mdp='topsecret_')            
+    port=os.environ.get('PORT',8080)
+    app.run(debug=False, host="0.0.0.0", port=port)
