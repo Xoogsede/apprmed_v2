bb4d5af7985ddacde0b3ba34b72fc903870013a5
Xoogsade <abdi-jama.mahamoud@hotmail.fr>
2022-08-20 10:04:57 +0200

    update form

--- app/auxsan/formulaire.py
+++ app/auxsan/formulaire.py
@@ -1,22 +1,24 @@
+from tkinter.tix import Select
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, RadioField, TextAreaField
+from wtforms import StringField, SubmitField, RadioField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
 
 
 class AjouterBlesse(FlaskForm):   
     
     matricule = StringField(render_kw={"placeholder": "Saisir ou scanner matricule"}, validators=[DataRequired()])
-    categorie_blesse = StringField(render_kw={"placeholder": "Categorie blesse"}, default= "C")
-    coordonnees_UTM_blesse = StringField(render_kw={"placeholder": "Coordonnees UTM"}, validators=[DataRequired()]) 
+    categorie_blesse = StringField("Catégorie blessé", render_kw={"placeholder": "Categorie blesse"}, default= "C")
+    coordonnees_UTM_blesse = StringField("Position du blessé", render_kw={"placeholder": "Coordonnées UTM"}, validators=[DataRequired()]) 
     symptomes = TextAreaField(render_kw={"placeholder": "Symptomes"})
-    blesse_couche = RadioField("Blessé couché ?", validators=[DataRequired()], choices=[(True, "Oui"), (False, "Non")])
+    blesse_couche = SelectField("Blessé couché ?", validators=[DataRequired()], choices=[(True, "Oui"), (False, "Non")])
     Valider = SubmitField('Valider')
 
 class MiseAJourblesse(FlaskForm):
 
     matricule = StringField(render_kw={"placeholder": "Saisir ou scanner matricule"}, validators=[DataRequired()])
-    categorie_blesse = StringField(render_kw={"placeholder": "Nouvelle catégorie"}, validators=[DataRequired()])
-    blesse_couche = RadioField(label="Blessé couché ?", validators=[DataRequired()], choices=[(True, "Oui"), (False, "Non")])
+    categorie_blesse = StringField("Catégorie blessé", render_kw={"placeholder": "Nouvelle catégorie"}, validators=[DataRequired()])
+    blesse_couche = SelectField("Blessé couché ?", validators=[DataRequired()], choices=[(True, "Oui"), (False, "Non")])
+    nouvelle_position = StringField("Nouvelle position", render_kw={"placeholder": "Nouvelles coordonnées UTM"}, validators=[DataRequired()])
     Valider = SubmitField('Valider')
 
 class MatriculeQRCode(FlaskForm):
