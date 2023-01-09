ec70361970757bcaf6620364763156567c41e778
Xoogsede <abdi-jama.mahamoud@hotmail.fr>
2023-01-09 01:37:15 +0100

    Ajout de commentaires aux différents fichier python

--- app/db_extention.py
+++ app/db_extention.py
@@ -1,3 +1,19 @@
+'''
+Ce code importe plusieurs modules et définit plusieurs variables qui seront utilisées dans 
+l'application Flask.
+
+    "SQLAlchemy" est une extension de Flask qui permet de gérer une base de données en utilisant le mappage objet-relationnel (ORM) de SQLAlchemy.
+    "Migrate" est une extension de Flask qui permet de gérer les migrations de base de données.
+    "automap_base" est une fonction de SQLAlchemy qui permet de générer automatiquement des classes de mappage de données à partir de tables de base de données existantes.
+    "Session" est une classe de SQLAlchemy qui permet de gérer une session de base de données.
+    "LoginManager" est une extension de Flask qui gère la gestion des connexions utilisateur.
+    "Bootstrap" est une extension de Flask qui ajoute le support de Bootstrap à l'application.
+    "Bcrypt" est une extension de Flask qui permet de crypter et de vérifier les mots de passe.
+
+Le code définit également une variable "ALLOWED_EXTENSIONS" qui contient une liste de types de 
+fichiers autorisés et une fonction "allowed_file" qui vérifie si un fichier a une extension autorisée.
+'''
+# Import des modules
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.ext.automap import automap_base
@@ -6,21 +22,38 @@ from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
 
+# Initialisation de SQLAlchemy
 db = SQLAlchemy()
 
-migrate = Migrate() # migration
-    
+# Initialisation de Migrate
+migrate = Migrate()
+
+# Création de la base de mappage de données
 Base = automap_base()
 
+# Initialisation de Bootstrap
 bootstrap = Bootstrap() 
 
+# Initialisation de LoginManager
 log_manager = LoginManager()
+# Configuration de la vue de connexion
 log_manager.login_view = 'authentication.login'
+# Configuration de la protection de la session
 log_manager.session_protection = 'strong'
 
+# Initialisation de Bcrypt
 bcrypt = Bcrypt()
 
+# Définition des types de fichiers autorisés
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    """
+    Vérifie si un fichier a une extension autorisée.
+    
+    :param filename: Nom du fichier à vérifier.
+    :return: True si le fichier a une extension autorisée, False sinon.
+    """
+    # Vérifie si le fichier a une extension et si cette extension est autorisée
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
+'''Ce code importe plusieurs modules et définit une variable uri qui contient l'URI de la base de données. 
+Si uri existe et commence par "postgres://", il est remplacé par "postgresql+psycopg2://".
+'''
+# Import des modules
 from werkzeug.security import generate_password_hash as gph, check_password_hash as cph
 from flask import Flask
-# from config import *
 from datetime import *
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
@@ -9,29 +12,62 @@ from flask_login import UserMixin
 from sqlalchemy.orm import Session
 import os
 
-
+# Récupération de l'URI de la base de données depuis les variables d'environnement
 uri = os.environ["DATABASE_URL"]  
+
+# Si l'URI existe et commence par "postgres://", remplacer par "postgresql+psycopg2://"
 if uri and uri.startswith("postgres://"): 
     uri = uri.replace("postgres://", "postgresql+psycopg2://")
 
 
-#########################################
-# BASE DE DONNEES StageM2_BD ############
-#########################################
+
+'''Ce code permet de créer une connexion à une base de données à l'aide de l'uri spécifié 
+et de réfléchir à la structure de la base de données. Ensuite, une session est créée à 
+partir de l'objet moteur pour permettre l'exécution de requêtes sur la base de données.
+'''
+# Création de l'objet moteur en utilisant l'uri de la base de données
 engine = create_engine(uri)
+
+# Préparation de la base de données en réfléchissant à sa structure
+# Cela crée des classes de mappage de données pour chaque table de la base de données
 Base.prepare(engine, reflect=True)
+
+# Création d'une session à partir de l'objet moteur
+# Cela permettra d'exécuter des requêtes sur la base de données
 session = Session(engine)
+
 
 
 #########################################
 # Utilisateurs Auxiliaire sanitaire
 #########################################
+'''
+Ce code définit une fonction "load_user" qui est utilisée comme chargeur d'utilisateur par le 
+gestionnaire de connexion de Flask-Login. La fonction prend un identifiant d'utilisateur en 
+entrée et renvoie l'objet utilisateur correspondant s'il existe.
+
+Le code définit également une classe "User" qui représente un utilisateur de l'application. 
+La classe hérite de "db.Model" et "UserMixin", qui sont des classes de base de données et de 
+gestion de connexion de Flask-Login, respectivement. La classe définit plusieurs colonnes de 
+base de données qui seront utilisées pour stocker les informations de l'utilisateur, telles 
+que l'identifiant, le matricule, la fonction, le hash du mot de passe et la date d'inscription.
+
+La classe "User" définit également une méthode "check_password" qui prend un mot de passe en entrée 
+et vérifie si il correspond au mot de passe hashé stocké dans la base de données.
+
+La classe "User" définit également une méthode de classe "create_user" qui prend en entrée un 
+matricule, une fonction et un mot de passe, et crée un nouvel objet utilisateur en utilisant 
+ces informations. Le mot de passe est hashé avant d'être stocké dans la base de données. 
+L'objet utilisateur est ensuite ajouté à la session de base de données et la transaction est validée.
+
+'''
 @log_manager.user_loader
 def load_user(user_id):
+    # Chargement de l'utilisateur à partir de la base de données en utilisant l'identifiant d'utilisateur
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-
+    # Définition de la table et des colonnes de la base de données
     __tablename__ = 'users'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer(), primary_key = True)
@@ -40,27 +76,40 @@ class User(db.Model, UserMixin):
     mdp_hash = db.Column(db.String(128), nullable = False)
     dateinscription = db.Column(db.DateTime, default=datetime.now) 
 
-    
+    # Méthode pour vérifier si un mot de passe entré correspond au mot de passe hashé stocké dans la base de données
     def check_password(self, mdp):
         return cph(self.mdp_hash, mdp)
 
+    # Méthode de classe pour créer un nouvel utilisateur en utilisant le matricule, la fonction et le mot de passe donnés
     @classmethod
     def create_user(cls, matricule, fonction, mdp):
-
+        # Création de l'objet utilisateur en hashant le mot de passe
         user = cls(matricule = matricule, 
                    fonction = fonction,    
                    mdp_hash = gph(mdp))
 
+        # Ajout de l'objet utilisateur à la session de base de données et validation de la transaction
         db.session.add(user)
         db.session.commit()
         return user
 
 
 
-#########################################
-# TABLES (MODELS) #######################
-#########################################
 
+#########################################
+# TABLES (MODELS) 
+#########################################
+'''
+Ce code définit plusieurs variables qui font référence à des classes de mappage de données générées 
+par SQLAlchemy. Ces classes sont utilisées pour représenter des tables de la base de données et 
+permettent de manipuler les données de la base de données en utilisant des objets Python.
+
+Chaque variable correspond à une classe de mappage de données qui a le même nom que la table 
+correspondante dans la base de données. Par exemple, la variable "armee" fait référence à une 
+classe de mappage de données qui représente la table "armee" de la base de données.
+'''
+
+# Définition de variables qui font référence à des classes de mappage de données
 accueil_blesse_en_zfsan = Base.classes.accueil_blesse_en_zfsan
 alertes_niveaux_produit = Base.classes.alertes_niveaux_produit
 armee                   = Base.classes.armee                  
@@ -97,11 +146,31 @@ en_soin                 = Base.classes.en_soin
 vt_en_mission           = Base.classes.vt_en_mission 
 
 
+
+'''
+La fonction "showblesse" prend en entrée une table de blessures (représentée par la variable "maTable") 
+et retourne une liste de listes contenant les informations de chaque blessure.
+
+La fonction parcourt chaque instance de "maTable" et extrait certaines informations, telles que le 
+matricule, la catégorie ABC, les coordonnées UTM de la blessure, l'heure de la blessure et l'heure 
+de l'évacuation. Ces informations sont ajoutées à une liste qui est ensuite ajoutée à la liste de lignes. 
+La fonction retourne la liste de lignes une fois qu'elle a parcouru toutes les instances de "maTable".
+'''
 def showblesse(maTable):
+    # Liste vide pour stocker les lignes de sortie
     lignes = []
+    
+    # Pour chaque instance de maTable, extraire certaines informations et les ajouter à une ligne
     for instance in maTable:
+        # Conversion de l'heure de la blessure et de l'heure de l'évacuation en chaîne de caractères
         gdhblessure = str(instance.gdhblessure)
         gdhevacue = str(instance.gdhevacue)
+        
+        # Création de la ligne avec les informations extraites
         bles = [instance.matricule, instance.categorieabc, instance.coordonneesutmblesse, gdhblessure, gdhevacue, instance.unite_elementaire]
+        
+        # Ajout de la ligne à la liste de lignes
         lignes.append(bles)
+    
+    # Retour de la liste de lignes
     return lignes
