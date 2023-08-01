2cbc78e84fed5cf36e20697e9e468ee6719454c4
Xoogsede <abdi-jama.mahamoud@hotmail.fr>
2023-08-02 00:41:07 +0200

    Mise api

--- app/auth/routes_auth.py
+++ app/auth/routes_auth.py
@@ -101,10 +101,12 @@ def login_mobile():
 
     if user and user.check_password(password):
         token = create_access_token(identity=matricule)  # create a token and send it back
+        # if not user.is_password_changed:
+        #     return jsonify({'status': 'success', 'message': 'change', 'matricule': user.matricule, 'fonction': user.fonction, 'change_password': True, 'token': token}), 200
+        
         return jsonify({'status': 'success', 'message': str(user.is_password_changed), 'matricule': user.matricule, 'fonction': user.fonction, 'token': token}), 200
 
     return jsonify({'status': 'error', 'message': 'Matricule ou mot de passe incorrect'}), 400
-
 
 
 @at.route('/change_password', methods=['POST'])
